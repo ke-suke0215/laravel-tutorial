d64b10ea0f81c461189bddfe347d3b43e0d2a089
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2022-03-13 21:42:30 +0900

    ログインしていないとアクセスできないよう設定

--- laravel-note-app/routes/web.php
+++ laravel-note-app/routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/create', [App\Http\Controllers\HomeController::class, 'create'])->name('create');
-Route::post('/store', [App\Http\Controllers\HomeController::class, 'store'])->name('store');
-// {}でコントローラーに中身をURLパラメータとして渡せる
-Route::get('/edit/{id}',[App\Http\Controllers\HomeController::class, 'edit'])->name('edit');
-Route::post('/update/{id}', [App\Http\Controllers\HomeController::class, 'update'])->name('update');
-Route::post('/delete/{id}', [App\Http\Controllers\HomeController::class, 'delete'])->name('delete');
+// ログインしていないと入れないようにする
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/create', [App\Http\Controllers\HomeController::class, 'create'])->name('create');
+    Route::post('/store', [App\Http\Controllers\HomeController::class, 'store'])->name('store');
+    // {}でコントローラーに中身をURLパラメータとして渡せる
+    Route::get('/edit/{id}',[App\Http\Controllers\HomeController::class, 'edit'])->name('edit');
+    Route::post('/update/{id}', [App\Http\Controllers\HomeController::class, 'update'])->name('update');
+    Route::post('/delete/{id}', [App\Http\Controllers\HomeController::class, 'delete'])->name('delete');
+});
