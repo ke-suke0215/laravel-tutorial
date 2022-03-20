68ead77047109f59237fdc978bb5c6f549802cf7
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2022-03-20 10:23:36 +0900

    属性バック

--- umarche/resources/views/tests/component-test1.blade.php
+++ umarche/resources/views/tests/component-test1.blade.php
@@ -4,7 +4,9 @@
   </x-slot>
   コンポーネントテスト1
 
-  <x-tests.card title="タイトル" content="コンテンツ" :message="$message"/>
-  <x-tests.card title="タイトル" />
+  <x-tests.card title="タイトル1" content="コンテンツ" :message="$message"/>
+  <x-tests.card title="タイトル2" />
+  <!-- タイトル3はCSSを変更したい -->
+  <x-tests.card title="タイトル3" class="bg-red-300" />
 
 </x-tests.app>
