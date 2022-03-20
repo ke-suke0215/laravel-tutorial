20ded9704cb031bfe06d7b0960758dc0bb437ef9
ke-suke0215 <keisuke.yohs.0215.1209@gmail.com>
2022-03-20 10:58:33 +0900

    classBase

--- umarche/resources/views/tests/component-test2.blade.php
+++ umarche/resources/views/tests/component-test2.blade.php
@@ -3,4 +3,8 @@
     ヘッダー2
   </x-slot>
   コンポーネントテスト2
+  <div class="mb-4"></div>
+  <x-test-class-base classBaseMessage="クラスベースのメッセージ" />
+  <div class="mb-4"></div>
+  <x-test-class-base classBaseMessage="クラスベースのメッセージ" defaultMessage="デフォルトのメッセージは変更"/>
 </x-tests.app>
